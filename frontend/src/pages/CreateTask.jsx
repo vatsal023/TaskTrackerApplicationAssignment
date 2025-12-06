9b2f53b907c31dc3a22a2a95cd9cbf211106b1f6
Vatsal Garg <144517333+vatsal023@users.noreply.github.com>
2025-12-06 10:45:02 +0530

    feat(ui): created task page

---
+++ frontend/src/pages/CreateTask.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+// import { AuthContext } from "../context/AuthContext";
+// import { createTask } from "../services/api";
+import TaskForm from "../components/TaskForm";
+import axios from "axios";
+import { useAuth } from "../context/authContext";
+import { useEffect } from "react";
+
+const CreateTask = () => {
+//   const { token } = useContext(AuthContext);
+  const navigate = useNavigate();
+     const { isAuthenticated, checkAuth } = useAuth();
+  
+    useEffect(() => {
+          
+            // checkAuth();
+            if (!isAuthenticated) {
+                navigate("/login");
+            }
+        }, [])
+
+  const handleSubmit = async (data) => {
+    await axios.post("/api/tasks",data);
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Create Task</h2>
+      <TaskForm onSubmit={handleSubmit} />
+    </div>
+  );
+};
+
+export default CreateTask;
