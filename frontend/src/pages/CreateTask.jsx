3c12ad31b169fa3ae3aaf85112de8ce3c5614c5b
Vatsal Garg <144517333+vatsal023@users.noreply.github.com>
2025-12-10 10:34:20 +0530

    update createtask

--- frontend/src/pages/CreateTask.jsx
+++ frontend/src/pages/CreateTask.jsx
@@ -13,16 +13,16 @@ const CreateTask = () => {
      const { isAuthenticated, checkAuth, authChecked } = useAuth();
   
     // First, check authentication status on mount
-    useEffect(() => {
-        checkAuth();
-    }, []);
+    // useEffect(() => {
+    //     checkAuth();
+    // }, []);
 
-    // Then, redirect if not authenticated (only after auth check completes)
-    useEffect(() => {
-        if (authChecked && !isAuthenticated) {
-            navigate("/login", { replace: true });
-        }
-    }, [isAuthenticated, authChecked, navigate]);
+    // // Then, redirect if not authenticated (only after auth check completes)
+    // useEffect(() => {
+    //     if (authChecked && !isAuthenticated) {
+    //         navigate("/login", { replace: true });
+    //     }
+    // }, [isAuthenticated, authChecked, navigate]);
   
   //submitting new task data
   const handleSubmit = async (data) => {
